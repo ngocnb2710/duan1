a6280c091d4cf1b132813ac5fb0b4576c4c4c17a
Ngoc Nguyen <ngoc.nguyen@solislab.com>
2020-07-26 16:17:16 +0700

    first commit

---
+++ database/migrations/2020_07_26_083918_create_invoice_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('invoice_email');
+            $table->text('address');
+            $table->string('phone_number');
+            $table->text('total');
+            $table->string('customer_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
---
+++ database/migrations/2020_07_26_083938_create_invoice_detail_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoiceDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_detail', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quatity');
+            $table->integer('product_id');
+            $table->string('price');
+            $table->integer('invoice_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_detail');
+    }
+}
